fdfc7bf3b02aedb2a5074c890e24ddaf0dcd2023
mokter7082 <mokter7082@gmail.com>
2023-08-03 22:47:42 +0600

    add cors package

--- src/app.js
+++ src/app.js
@@ -3,12 +3,11 @@ const express = require("express");
 const morgan = require("morgan");
 const createError = require("http-errors");
 const userRouter = require("./router/user.route");
-const { tokenVerify } = require("./middleware/verify_token");
+const cors = require('cors');
 
 const app = express();
 app.use(morgan());
 app.use(express.json());
-// app.use(tokenVerify);
 
 app.get("/", (req, res) => {
   res.send("API server working !");
